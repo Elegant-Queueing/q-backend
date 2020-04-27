baff1ff1538e232bf5953aa6d13c32c68675b60b
Sarvagya Gupta <guptasarvagyav@gmail.com>
2020-04-26 20:15:12 -0700

    first commit

---
+++ src/test/java/com/careerfair/q/QApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerfair.q;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
