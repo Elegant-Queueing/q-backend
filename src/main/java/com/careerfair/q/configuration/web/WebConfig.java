fc93016f8289851d613e5f83020ad7e4a14ad06f
Sarvagya Gupta <guptasarvagyav@gmail.com>
2020-05-12 14:22:09 -0700

    cross origin updated

--- src/main/java/com/careerfair/q/configuration/web/WebConfig.java
+++ src/main/java/com/careerfair/q/configuration/web/WebConfig.java
@@ -1,12 +1,12 @@
 package com.careerfair.q.configuration.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+@CrossOrigin
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
