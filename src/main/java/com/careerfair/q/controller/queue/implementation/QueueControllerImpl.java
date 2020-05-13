677cc7e3aeb1357c40a1fcd23092da3b61f42b62
Sarvagya Gupta <guptasarvagyav@gmail.com>
2020-05-12 17:23:08 -0700

    pls work

--- src/main/java/com/careerfair/q/controller/queue/implementation/QueueControllerImpl.java
+++ src/main/java/com/careerfair/q/controller/queue/implementation/QueueControllerImpl.java
@@ -91,7 +91,7 @@ public class QueueControllerImpl implements QueueController {
         return queueService.registerStudent(employeeId, studentId);
     }
 
-    @DeleteMapping("/remove-student/employee-id/{employee-id}/student-id/{student-id}")
+    @PostMapping("/remove-student/employee-id/{employee-id}/student-id/{student-id}")
     @Override
     public RemoveStudentResponse removeStudent(@PathVariable("employee-id") String employeeId,
                                                @PathVariable("student-id") String studentId) {
