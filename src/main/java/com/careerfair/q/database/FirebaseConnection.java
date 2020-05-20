69ca8f19da53665aedbd9663a66deebfa4c504b0
Sarvagya Gupta <guptasarvagyav@gmail.com>
2020-05-19 23:06:19 -0700

    Added db url as property

--- src/main/java/com/careerfair/q/database/FirebaseConnection.java
+++ src/main/java/com/careerfair/q/database/FirebaseConnection.java
@@ -3,6 +3,8 @@ package com.careerfair.q.database;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -10,7 +12,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 @Service
+@PropertySource(value = "application.properties")
 public class FirebaseConnection {
+
+    @Value("${db.url}")
+    private String url;
 
     @PostConstruct
     public void init() throws IOException {
@@ -19,7 +25,7 @@ public class FirebaseConnection {
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://q-firebase-98f6e.firebaseio.com")
+                .setDatabaseUrl(url)
                 .build();
 
         FirebaseApp.initializeApp(options);
