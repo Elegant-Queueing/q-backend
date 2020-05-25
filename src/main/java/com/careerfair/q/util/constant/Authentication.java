5458ce0908ceb8f84822415dc3e1584b90c7803d
Sachin Bhargava <sachin20@uw.edu>
2020-05-24 19:38:11 -0700

    implemented authentication

---
+++ src/main/java/com/careerfair/q/util/constant/Authentication.java
@@ -0,0 +1,9 @@
+package com.careerfair.q.util.constant;
+
+public class Authentication {
+
+    /**
+     * This token must be passed in all API calls as a header
+     */
+    public static final String TOKEN = "token";
+}
