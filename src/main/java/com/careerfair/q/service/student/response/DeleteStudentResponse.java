0633c311b4d6476bcf6bbe22f8640608a5ce5e4b
Aman Agarwal <aman_k_agarwal@hotmail.com>
2020-05-30 02:17:43 -0700

    fixed @EqualsHashCode warning

--- src/main/java/com/careerfair/q/service/student/response/DeleteStudentResponse.java
+++ src/main/java/com/careerfair/q/service/student/response/DeleteStudentResponse.java
@@ -3,10 +3,12 @@ package com.careerfair.q.service.student.response;
 import com.careerfair.q.model.db.Student;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class DeleteStudentResponse extends StudentResponse {
 
     @JsonProperty("student_id")
