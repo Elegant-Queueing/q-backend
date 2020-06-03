5298126d02cce37839f43cca7094912037bcd08e
Aman Agarwal <aman_k_agarwal@hotmail.com>
2020-06-02 17:10:53 -0700

    swagger config

--- src/main/java/com/careerfair/q/configuration/swagger/SwaggerConfiguration.java
+++ src/main/java/com/careerfair/q/configuration/swagger/SwaggerConfiguration.java
@@ -3,6 +3,7 @@ package com.careerfair.q.configuration.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -14,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration implements WebMvcConfigurer {
+public class SwaggerConfiguration extends WebMvcConfigurationSupport {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -24,7 +25,7 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
                 .build();
     }
 
-    private ApiInfo getApiInfo() {
+    private ApiInfo metaData() {
 
         return new ApiInfoBuilder()
                 .title("Q-Backend Swagger API Doc")
@@ -32,4 +33,13 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
                 .version("1.0.0")
                 .build();
     }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 }
